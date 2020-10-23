0dfd7e28dff0ca4c9c17749f1608d89d6194996b
failedcoder12 <divyanshverma12@gmail.com>
2020-10-24 00:10:29 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema')
+const mongoose = require("mongoose");
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+const mongooseConnection = "your connectionstring";
+
+mongoose.connect(mongooseConnection,(err) => {
+    if(err) console.log(err);
+});
+
+mongoose.connection.once('open', () => {
+    console.log("connected to database");
+});
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
